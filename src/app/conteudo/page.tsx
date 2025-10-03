9dafc15f636b5c5cd5fe87d9585555965934cf38
Lohan Santos <lohansantosborges@gmail.com>
2025-10-03 22:07:05 +0000

    ⚠ You are using a non-standard "NODE_ENV" value in your environment. Thi

--- src/app/conteudo/page.tsx
+++ src/app/conteudo/page.tsx
@@ -1,6 +1,9 @@
 "use client";
 import ConteudoEstudo from "@/components/pages/ConteudoEstudo";
 
+// Adicione esta linha para forçar a renderização dinâmica
+export const dynamic = 'force-dynamic';
+
 export default function ConteudoPage() {
   return <ConteudoEstudo />;
 }
