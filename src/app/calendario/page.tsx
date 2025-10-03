03903f51eb5142e2eecc0ce48156737e5be5778c
Lohan Santos <lohansantosborges@gmail.com>
2025-10-03 22:01:04 +0000

    Agora o log de build nos mostra dois novos problemas em páginas diferent

--- src/app/calendario/page.tsx
+++ src/app/calendario/page.tsx
@@ -1,6 +1,9 @@
 "use client";
 import Calendario from "@/components/pages/Calendario";
 
+// Adicione esta linha para forçar a renderização dinâmica
+export const dynamic = 'force-dynamic';
+
 export default function CalendarioPage() {
   return <Calendario />;
 }
