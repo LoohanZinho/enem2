29a37fb59d8b7e065d7851c3b11e37622d7f3b25
Lohan Santos <lohansantosborges@gmail.com>
2025-10-04 03:09:38 +0000

    cliquei aqui porem nao fui redirecionado pra /cronograma (_for element s

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,6 @@
 "use client";
 import Login from "@/components/pages/Login";
 
-// Adicione esta linha para forçar a renderização dinâmica
-export const dynamic = 'force-dynamic';
-
 export default function LoginPage() {
   return <Login />;
 }
